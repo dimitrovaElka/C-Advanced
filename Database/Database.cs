962d5cd7859eebc8aa416d0235934e86df9545fd
Elka Dimitrova <dimitrova_elka@abv.bg>
2018-04-08 19:53:43 +0300

    Database

--- Database/Database.cs
+++ Database/Database.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
-namespace Database
+namespace Db
 {
     public class Database : IDatabase
     {
@@ -10,14 +11,61 @@ namespace Database
         private int[] data;
         private int currentIndex;
 
+        private Database()
+        {
+            this.Data = new int[capacity];
+            this.currentIndex = 0;
+        }
+        public Database(params int[] elements)
+            : this()
+        {
+            NewMethod(elements);
+        }
+
+        private void NewMethod(int[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                this.Data[i] = elements[i];
+            }
+            this.currentIndex = elements.Length;
+        }
+
+        public int[] Data
+        {
+            get
+            {
+                return this.data;
+            }
+            private set
+            {
+                if (value.Length < 1 || value.Length > capacity)
+                {
+                    throw new InvalidOperationException();
+                }
+                this.data = value;
+            }
+        }
         public void Add(int element)
         {
-            throw new NotImplementedException();
+            if (this.currentIndex >= capacity)
+            {
+                throw new InvalidOperationException();
+            }
+            this.Data[currentIndex] = element;
+            this.currentIndex++;
         }
 
         public int Remove()
         {
-            throw new NotImplementedException();
+            if (currentIndex <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            this.currentIndex--;
+            int element = data[currentIndex];
+            data[currentIndex] = default(int); 
+            return element;
         }
     }
 }
--- DatabaseTests/DataTests.cs
+++ DatabaseTests/DataTests.cs
@@ -1,13 +1,31 @@
-﻿using NUnit.Framework;
-using System;
+﻿using Db;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace DatabaseTests
 {
     public class DataTests
     {
+    //    [Test]
+    //    public void ShouldElementAddAtTheNextFreeCell()
+    //    {
+
+    //    }
+
         [Test]
-        public void ShouldElementAddAtTheNextFreeCell()
+       
+        public void TestValidConstructor(int[] values)
         {
+            Database db = new Database(values);
+            //FieldInfo fieldInfo = typeof(Database).GetFields()
+            //    .First(fi => fi.FieldType == typeof(int[]));
+            //IEnumerable<int> fieldValues = fieldInfo.GetValue(db);
+            for (int i = 0; i < values.Length; i++)
+            {
+                Assert.That(db.Data[i], Is.EqualTo(values[i]));
+            }
 
         }
     }
